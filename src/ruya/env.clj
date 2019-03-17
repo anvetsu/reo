42ac9915bfbe083327142750876cc785351a7bb6
Vijay Mathew <mathew.vijay@gmail.com>
2019-03-17 19:18:14 +0530

    environment and evaluator - first commit

---
+++ src/ruya/env.clj
@@ -0,0 +1,17 @@
+(ns ruya.env)
+
+(defn make []
+  {:parent nil
+   :bindings {}})
+
+(defn amend [env var val]
+  (let [b (:bindings env)]
+    (assoc env :bindings (assoc b var val))))
+
+(defn lookup [env var]
+  (loop [env env]
+    (let [b (:bindings env)]
+      (if-let [val (get b var)]
+        val
+        (when-let [p (:parent env)]
+          (recur p))))))
---
+++ src/ruya/eval.clj
@@ -0,0 +1,23 @@
+(ns ruya.eval
+  (:require [ruya.env :as env]))
+
+(defn- literal [x]
+  (or (number? x)
+      (string? x)
+      (boolean? x)))
+
+(declare evaluate)
+
+(defn- amend [var val-expr env]
+  (let [val (evaluate val-expr env)]
+    [val (env/amend env var val)]))
+
+(defn- eval-form [ident args env]
+  (case ident
+    :define (amend (first args) (second args) env)))
+
+(defn evaluate [expr env]
+  (cond
+    (literal? expr) [expr env]
+    (symbol? expr) [(env/lookup env expr) env]
+    (seq expr) (eval-form (first expr) (rest expr) env)))
