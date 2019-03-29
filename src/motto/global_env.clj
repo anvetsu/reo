c2f48acf20d045986f94a11a3f055c67abca597d
Vijay Mathew <mathew.vijay@gmail.com>
2019-03-29 17:28:03 +0530

    built-in folds

--- src/motto/global_env.clj
+++ src/motto/global_env.clj
@@ -1,8 +1,8 @@
 (ns motto.global-env
-  (:require [motto.env :as env]
-            [motto.compile :as c]
-            [motto.lib.list :as mll]
-            [motto.lib.burrow :as mlb]))
+  (:require [motto.compile]
+            [motto.lib.burrow]
+            [motto.lib.num]
+            [motto.lib.list]))
 
 (defn make-eval []
   (eval
@@ -17,13 +17,23 @@
       (def ->=- motto.lib.burrow/gteq)
       (def -<=- motto.lib.burrow/lteq)
       (def <> motto.lib.burrow/neq)
+
       (def -get- get)
       (def -neg- -)
+      (def big motto.lib.num/big)
+      (def sml motto.lib.num/small)
+
       (def c motto.compile/compile-file)
       (def -conj- motto.lib.list/-conj-)
       (def -fold- motto.lib.list/-fold-)
       (def -map- motto.lib.list/-map-)
-      (def -fold-incr- motto.lib.list/-fold-incr-)
+      (def -fold-incr- motto.lib.list/fold-incr)
+      (def sum motto.lib.list/sum)
+      (def dif motto.lib.list/diff)
+      (def prd motto.lib.list/prd)
+      (def qt motto.lib.list/-quot-)
+      (def mx motto.lib.list/-max-)
+      (def mn motto.lib.list/-min-)
       (def til motto.lib.list/til)
 
       (fn [expr] (eval expr)))))
--- src/motto/lib/list.clj
+++ src/motto/lib/list.clj
@@ -1,4 +1,6 @@
-(ns motto.lib.list)
+(ns motto.lib.list
+  (:require [motto.lib.burrow :as b]
+            [motto.lib.num :as n]))
 
 (defn til [x]
   (into []
@@ -23,9 +25,27 @@
       (recur (rest xs) (conj rs (f (first xs))))
       rs)))
 
-(defn -fold-incr- [ys f]
+(defn fold-incr [ys f]
   (loop [xs (rest ys), lv (first ys), r [lv]]
     (if (seq xs)
       (let [cv (f lv (first xs))]
         (recur (rest xs) cv (conj r cv)))
       r)))
+
+(defn sum [ys]
+  (-fold- ys b/add))
+
+(defn diff [ys]
+  (-fold- ys b/sub))
+
+(defn prd [ys]
+  (-fold- ys b/mul))
+
+(defn -quot- [ys]
+  (-fold- ys b/div))
+
+(defn -max- [ys]
+  (-fold- ys n/big))
+
+(defn -min- [ys]
+  (-fold- ys n/small))
