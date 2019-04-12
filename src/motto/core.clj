04ecdd7669ceddb2d3f5ccc9f4bf3f2fe6f7cac3
Vijay Mathew <mathew.vijay@gmail.com>
2019-04-12 11:29:47 +0530

    start repl by default

--- src/motto/core.clj
+++ src/motto/core.clj
@@ -19,10 +19,13 @@
       (shutdown)))))
 
 (def cli-opts [["-s" "--server" "HTTP server mode"]
-               ["-p" "--port" "HTTP server port"
+               ["-p" "--port PORT" "HTTP server port"
                  :default 3030
                 :parse-fn #(Integer/parseInt %)
                 :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+               ["-r" "--repl FLAG" "start the repl. FLAG must be either true or false"
+                :default true
+                :parse-fn #(Boolean/valueOf %)]
                ["-V" "--verbose" "verbose output"]
                ["-v" "--version" "show version"]
                ["-h" "--help" "print this help"]])
@@ -47,6 +50,7 @@
       (:version opts) (show-version! true)
       (:help opts) (show-help! opts)
       (:verbose opts) (f/verbose!))
-    (if (:server opts)
-      (httpd/start (:port opts))
+    (when (:server opts)
+      (httpd/start (:port opts)))
+    (when (:repl opts)
       (repl))))
