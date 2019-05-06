eec7094cce5903f91165500f8a1b2ccb42686066
Vijay Mathew <mathew.vijay@gmail.com>
2019-05-06 13:46:19 +0530

    xls as tab

--- src/motto/lib/xls.clj
+++ src/motto/lib/xls.clj
@@ -1,6 +1,13 @@
 (ns motto.lib.xls
   (:require [incanter.core :as ic]
-            [incanter.excel :as ixls]))
+            [incanter.excel :as ixls]
+            [motto.lib.tab :as tab]))
+
+(defn- dset->tab [dset]
+  (let [colnames (map symbol (:column-names dset))
+        cols (:columns dset)]
+    (tab/mktab colnames cols)))
 
 (defn load-data [filename]
-  (ixls/read-xls filename))
+  (let [dset (ixls/read-xls filename)]
+    (dset->tab dset)))
