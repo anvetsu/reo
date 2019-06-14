f27a58bd30c61fc42aa3a76b0eae1789a277e73a
Vijay Mathew <mathew.vijay@gmail.com>
2019-06-14 21:34:15 +0530

    #5 - customizable prompts

---
+++ src/motto/config.clj
@@ -0,0 +1,14 @@
+(ns motto.config)
+
+(def ^:private prompt-str (atom "=> "))
+(def ^:private prompt2-str (atom "- "))
+
+(defn prompt [] @prompt-str)
+
+(defn prompt! [s]
+  (reset! prompt-str s))
+
+(defn prompt2 [] @prompt2-str)
+
+(defn prompt2! [s]
+  (reset! prompt2-str s))
